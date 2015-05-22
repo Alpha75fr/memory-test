222a2202e989d9bc80cc09782efba57f1cf5c377
pascal <pascal@free.fr>
2015-05-22 16:55:20 +0200

    Commit initial du projet memory-test-01

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+
+public class Main {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		ArrayList<Long> list = new ArrayList<Long>();
+		Thread.sleep(5000); // 5s to allow starting visualvm
+		for (long l = 0; l < Long.MAX_VALUE; l++) {
+			list.add(new Long(l));
+			for (int i = 0; i  < 5000; i++) {
+				// busy wait, 'cause 1ms sleep is too long
+				if (i == 5000) break;
+			}
+		}
+	}
+
+}
